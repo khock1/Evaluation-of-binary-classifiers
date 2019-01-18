1e30449c857de54d5c9c2090acb637454efc8ac4
khock1 <45620184+khock1@users.noreply.github.com>
2019-01-18 16:26:43 +1000

    Update binary_classifier.m

--- binary_classifier.m
+++ binary_classifier.m
@@ -11,15 +11,15 @@
 
 n_measures = 200;% number of measurements in a sample
 n_predictions = 100;% number of times a sequence of measures representing predicted condition has been generated for evaluation
-x = datasample(0:1, n_measures);% true condition
-y = zeros(n_predictions, n_measures);% container to store predicted conditions
+x = datasample(0:1, n_measures);% 'true condition'
+y = zeros(n_predictions, n_measures);% container to store 'predicted conditions'
 
 
 % CREATE DUMMY DATA TO USE AS 'PREDICTED CONDITIONS'-----------------------------
 
-% simple way to create dummy data; use the true condtion sequence, and
-% switch values with some random chance to obtain valus for a 'predicted condition'; 
-% the less values switch, the more like true condition this sequence will be
+% simple way to create dummy data; use the 'true condition' binary sequence, and
+% switch binary values with some constant chance to obtain valus for a 'predicted condition'; 
+% the less likely the values are to switch, the more like true condition this sequence will be
 perm_chance = zeros(n_predictions,1);
 for prediction = 1:n_predictions
     perm_chance(prediction, 1) = rand;
@@ -30,8 +30,7 @@ for prediction = 1:n_predictions
             end
         else
             y(prediction, measure) = x(measure);
-        end
-        
+        end 
     end
 end
 
@@ -40,35 +39,35 @@ end
 % container for storing classifier evaluation metrics
 classifier = struct('prediction',[],'confusion_matrix',[],'precision',[],'sensitivity',[],'accuracy',[],'specificity',[],'informedness',[],'MCC',[],'F1score',[],'Fisher_significance',[],'DOR',[],'false_pos_rate',[], 'false_neg_rate',[], 'error',[]);
 
-% loop through the samples of predicted condtions to evaualte them
+% loop through the 'predicted conditions' to evaluate their performance in predicting 'true condition'
 for p = 1:n_predictions
     classifier(p).prediction = p;% record the number of samples
     confus_mat = zeros(2);% confusion matrix
-    for m = 1:n_measures% now, determine the match between true condition and predicted condition and build a confusion matrix out of that
+    for m = 1:n_measures% determine the match between true condition and predicted condition and build a confusion matrix out of that
         this_x = x(1, m);
         this_y = y(p, m);
         if this_x == 1
             if this_y == 1
-                confus_mat(1, 1) = confus_mat(1, 1)+1;%true positives
+                confus_mat(1, 1) = confus_mat(1, 1)+1;% true positives
             else
-                confus_mat(1, 2) = confus_mat(1, 2)+1;%false negatives
+                confus_mat(1, 2) = confus_mat(1, 2)+1;% false negatives
             end
         else
             if this_y == 1
-                confus_mat(2, 1) = confus_mat(2, 1)+1;%false positives
+                confus_mat(2, 1) = confus_mat(2, 1)+1;% false positives
             else
-                confus_mat(2, 2) = confus_mat(2, 2)+1;%true negatives
+                confus_mat(2, 2) = confus_mat(2, 2)+1;% true negatives
             end
         end
     end
-    % spell out the true and false positives and negatives for easier tracking
+    % extract the true and false positives and negatives for easier tracking
     true_positives = confus_mat(1, 1);
     false_positives = confus_mat(2, 1);% type I error
     true_negatives = confus_mat(2, 2);
     false_negatives = confus_mat(1, 2);% type II error
     
     % calculate and store a selection of statistics to evaluate peformance
-    % names of most statistics are self-explanatory or easily found in literature or Wikipedia
+    % names of most statistics are self-explanatory or easily found in literature (or on Wikipedia)
     classifier(p).confusion_matrix = confus_mat;
     classifier(p).precision = true_positives/(true_positives+false_positives);
     classifier(p).sensitivity = true_positives/(true_positives+false_positives);
@@ -92,11 +91,11 @@ end
 threshold = 0.95;% this is the performance that we want to get from the prediction(s); e.g. accuracy above this value is considered satisfactory
 bar([classifier.accuracy]);% bar plot of accuracy scores
 hold;
-plot(xlim, [threshold threshold], 'r')
+plot(xlim, [threshold threshold], 'r')% plot threshold value
 title('Accuracy performance', 'FontSize', 11);
-xlabel('Prediction No');
+xlabel('Prediction Number');
 ylabel('Accuracy');
-% find all predictions that perfomed above the threshold
-prediction_above_threshold=find([classifier.accuracy]>=threshold);
+% find all indices of predictions that perfomed above the threshold
+prediction_above_threshold = find([classifier.accuracy] >= threshold);
 % find the best perfoming prediction(s)
-best_prediction=find([classifier.accuracy]==max([classifier.accuracy]));
+best_prediction = find([classifier.accuracy] == max([classifier.accuracy]));
